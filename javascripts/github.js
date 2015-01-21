b93ae75d94cc536eacf45cb236ff23bbc0d8d5a9
vincyason <991875786@qq.com>
2015-01-21 14:54:23 +0800

    Site updated at 2015-01-21 06:54:23 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,32 +1,17 @@
 var github = (function(){
-<<<<<<< HEAD
-=======
-  function escapeHtml(str) {
-    return $('<div/>').text(str).html();
-  }
->>>>>>> 89d78678ccc40fa5c9e37628349cfdd0af7a5837
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-<<<<<<< HEAD
       fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+(repos[i].description||'')+'</p></li>';
-=======
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
->>>>>>> 89d78678ccc40fa5c9e37628349cfdd0af7a5837
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
       $.ajax({
-<<<<<<< HEAD
           url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
         , type: 'jsonp'
-=======
-          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
-        , dataType: 'jsonp'
->>>>>>> 89d78678ccc40fa5c9e37628349cfdd0af7a5837
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
@@ -35,7 +20,6 @@ var github = (function(){
             if (options.skip_forks && data.data[i].fork) { continue; }
             repos.push(data.data[i]);
           }
-<<<<<<< HEAD
           repos.sort(function(a, b) {
             var aDate = new Date(a.pushed_at).valueOf(),
                 bDate = new Date(b.pushed_at).valueOf();
@@ -44,8 +28,6 @@ var github = (function(){
             return aDate > bDate ? -1 : 1;
           });
 
-=======
->>>>>>> 89d78678ccc40fa5c9e37628349cfdd0af7a5837
           if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
         }
